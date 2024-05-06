88caefd107fa28b15d0124a57419453b54099599
M307 <me@m307.dev>
2024-05-06 17:02:00 +0700

    fix: Slip verify has panic

--- validate.go
+++ validate.go
@@ -8,6 +8,10 @@ type SlipVerifyStruct struct {
 func SlipVerify(payload string) *SlipVerifyStruct {
 	ppqr := Parse(payload, true, true)
 
+	if ppqr == nil {
+		return nil
+	}
+
 	apiType := ppqr.GetTagValue("00", "00")
 	sendingBank := ppqr.GetTagValue("00", "01")
 	transRef := ppqr.GetTagValue("00", "02")
